52af80707fbce6d03f3673295f77aa508110dc4a
Sami <coolsweetsami@gmail.com>
2023-05-12 14:01:40 -0400

    link to project 2 added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 const button = document.querySelector('#background1');
 
 button.addEventListener('click', function() {
+  window.location.href = 'https://mental-oasis.herokuapp.com/';
+});
+
+const button2 = document.querySelector('#background2');
+
+button.addEventListener('click', function() {
   window.location.href = 'https://cooljh2k.github.io/dis-cover-music/';
 });
